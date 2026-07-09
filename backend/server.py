2d1b1009a26b5ebfacacc87814f03c2acd2ca5d3
LioOnSpier <amogkharwar5521@gmail.com>
2026-07-09 19:02:43 +0530

    enviroment

--- backend/server.py
+++ backend/server.py
@@ -24,8 +24,8 @@ from starlette.middleware.cors import CORSMiddleware
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / ".env")
 
-MONGO_URL = os.environ["MONGO_URL"]
-DB_NAME = os.environ["DB_NAME"]
+MONGO_URL = os.environ["EXPO_PUBLIC_BACKEND_URL"]
+DB_NAME = "schoolbusfees"
 JWT_SECRET = os.environ["JWT_SECRET"]
 JWT_EXPIRY_HOURS = int(os.environ.get("JWT_EXPIRY_HOURS", "720"))
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "")
